c5649c1741d057bae22affea43724c3d2b88bd6c
徐响 <xuxiang@xuxiangdeMacBook-Pro.local>
2023-11-09 12:39:51 +0900

    add script for windows

---
+++ install.bat
@@ -0,0 +1,5 @@
+cd backend\UPC-API && npm install
+cd ..\..\
+cd frontend\UPC-react && npm install
+cd ..\..\
+cd register-server && npm install
---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+(cd backend/UPC-API && npm install)
+(cd frontend/UPC-react && npm install)
+(cd register-server && npm install)
