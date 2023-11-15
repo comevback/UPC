d72f3e3c582c052d6607ba6a9536f70b023d24bd
徐响 <xuxiang@xuxiangdeMacBook-Pro.local>
2023-11-16 05:23:32 +0900

    modify setArgs.sh install.sh

--- install.sh
+++ install.sh
@@ -10,9 +10,9 @@
 os_name=$(uname -s)
 
 #  get local ip address according to the operating system
-if [[ "$os_name" == "Linux" ]]; then
+if [ "$os_name" = "Linux" ]; then
     ip_address=$(hostname -I | awk '{print $1}')
-elif [[ "$os_name" == "Darwin" ]]; then
+elif [ "$os_name" = "Darwin" ]; then
     ip_address=$(ifconfig | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2}' | head -n 1)
 else
     echo "Unsupported OS"
@@ -24,12 +24,12 @@ echo "Local IP Address: $ip_address"
 backend_method="./backend/UPC-API/Components/methods.js"
 
 # replace the ip address in the methods.js file
-if [[ "$os_name" == "Linux" ]]; then
-    sed -i "s/const CENTRAL_SERVER = 'http:\/\/localhost:8000';/const CENTRAL_SERVER = 'http:\/\/${ip_address}:8000';/" $backend_method
-    sed -i "s/url: 'http:\/\/localhost:4000',/url: 'http:\/\/${ip_address}:4000',/" $backend_method
-    sed -i "s/const id = 'API Service PC 1';/const id = 'API Service: ${ip_address}';/" $backend_method
-elif [[ "$os_name" == "Darwin" ]]; then
-    sed -i '' "s/const CENTRAL_SERVER = 'http:\/\/localhost:8000';/const CENTRAL_SERVER = 'http:\/\/${ip_address}:8000';/" $backend_method
-    sed -i '' "s/url: 'http:\/\/localhost:4000',/url: 'http:\/\/${ip_address}:4000',/" $backend_method
-    sed -i '' "s/const id = 'API Service PC 1';/const id = 'API Service: ${ip_address}';/" $backend_method
+if [ "$os_name" = "Linux" ]; then
+    sed -i "s/const CENTRAL_SERVER = '.*';/const CENTRAL_SERVER = 'http:\/\/${ip_address}:8000';/" $backend_method
+    sed -i "s/url: '.*',/url: 'http:\/\/${ip_address}:4000',/" $backend_method
+    sed -i "s/const id = '.*';/const id = 'API: ${ip_address}';/" $backend_method
+elif [ "$os_name" = "Darwin" ]; then
+    sed -i '' "s/const CENTRAL_SERVER = '.*';/const CENTRAL_SERVER = 'http:\/\/${ip_address}:8000';/" $backend_method
+    sed -i '' "s/url: '.*',/url: 'http:\/\/${ip_address}:4000',/" $backend_method
+    sed -i '' "s/const id = '.*';/const id = 'API: ${ip_address}';/" $backend_method
 fi
